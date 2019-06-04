d56cbc17d374960c4f74e004c8e425d49954fb27
Harrison Chen <harrisonchen@harrisons-mbp.lan>
2019-06-04 07:26:11 -0700

    minor bug fixed

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const helmet = require('helmet')
 const {NODE_ENV} = require('./config')
 const weatherRouter = require('./Weather/weather-router')
 const authRouter = require('./auth/auth-router')
+const usersRouter = require('./users/users-router')
 
 const app = express()
 
@@ -21,6 +22,7 @@ app.get('/', (req,res) => {
 })
 app.use('/api/weather',weatherRouter);
 app.use('/api/auth',authRouter);
+app.use('/api/users', usersRouter);
 
 app.use(function errorHandler(error, req, res, next) {
     let response
--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -7,14 +7,15 @@ const jsonBodyParser = express.json()
 
 authRouter
   .post('/login', jsonBodyParser, (req, res, next) => {
-    const { user_name, password } = req.body
+    const { user_name, password } = req.query
     const loginUser = { user_name, password }
+    console.log(req.query)
 
     for (const [key, value] of Object.entries(loginUser))
       if (value == null)
         return res.status(400).json({
           error: `Missing '${key}' in request body`
-        })
+        }) 
 
     AuthService.getUserWithUserName(
       req.app.get('db'),
